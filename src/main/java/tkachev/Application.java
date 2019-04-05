d5bf1b40a1d7aa945acaecfd34b7fe6c269bcf55
lixon92 <47113834+lixon92@users.noreply.github.com>
2019-04-05 17:14:34 +0300

    Первый коммит в FirstBranch

--- src/main/java/tkachev/Application.java
+++ src/main/java/tkachev/Application.java
@@ -1,14 +1,7 @@
 package tkachev;
 
-import tkachev.model.Project;
-import tkachev.repository.ProjectRepository;
-import tkachev.service.ProjectService;
-
 public class Application {
     public static void main(String[] args) {
-        Project project = new Project();
-        ProjectRepository projectRepository = new ProjectRepository();
-        ProjectService projectService = new ProjectService();
-
+        System.out.println("First branch");
     }
 }
