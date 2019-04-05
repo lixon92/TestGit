9df0e2ba6f15a63f03af896cebcedd55f9055577
lixon92 <47113834+lixon92@users.noreply.github.com>
2019-04-05 18:06:01 +0300

    3 коммит в Second

--- src/main/java/tkachev/Application.java
+++ src/main/java/tkachev/Application.java
@@ -3,5 +3,6 @@ package tkachev;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Second branch");
+        System.out.println("new message");
     }
 }
