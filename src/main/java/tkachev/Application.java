3aadfd8c009f87e75e111a4d10331b3980ecce2c
lixon92 <47113834+lixon92@users.noreply.github.com>
2019-04-09 11:12:07 +0300

    изменение в App

--- src/main/java/tkachev/Application.java
+++ src/main/java/tkachev/Application.java
@@ -1,8 +1,18 @@
 package tkachev;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Second branch");
         System.out.println("new message");
+        int[] integerList = {1, 5, 9, 3};
+        Arrays.sort(integerList);
+
+        System.out.println(Arrays.toString(integerList));
+        System.out.println((Integer.MAX_VALUE - 1) / 2);
+        System.out.println(Integer.MAX_VALUE);
+
     }
 }
